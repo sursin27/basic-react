14629de0ef0561ea795e20016fbe8dfecdb55725
sursin27 <53503398+sursin27@users.noreply.github.com>
2019-12-29 23:38:36 +0530

    2nd

--- src/js/reducers/reducer2.js
+++ src/js/reducers/reducer2.js
@@ -2,7 +2,7 @@ let initial=0;
 export const IncreReducer=(state=initial,action)=>{
     switch(action.type){
         case 'increment':
-            return 27
+            return state+1;
         default:
             return 28
     }
